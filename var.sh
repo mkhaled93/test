f08a106d05141a41455c10277919759e8c29bea8
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-20 18:24:15 +0200

    ye

--- var.sh
+++ var.sh
@@ -1,6 +1,7 @@
     CC_API_KEY="25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
     CC_REGION="ap-southeast-2"
     CFN_TEMPLATE_FILE_LOCATION="https://github.com/mkhaled93/test/blob/master/s3.yml"
+    source ~/.bash_profile
     echo $CFN_TEMPLATE_FILE_LOCATION
     printenv
     python3 scanner.py
