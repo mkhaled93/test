adf70b924cab067ad0bebe6971e890d4cad6cd89
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-20 14:19:15 +0200

    ye

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ node {
     //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: BRANCH_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '.']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS, url: GIT_REPO]]]
 
     stage "Build the Environment"
-    sh "aws cloudformation --region us-west-2 create-stack --stack-name myteststack --template-body file://test/s3.yml"
+    sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststack --template-body file://test/s3.yml"
     /*sh "docker build -t $SCAN_REPOSITORY ."
     sh "printenv"
     stage "Send to Repository"
