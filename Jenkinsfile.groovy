73ac4b2e124cf9d020d34b1e2fc1213d50e2a82d
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-20 18:32:07 +0200

    ye

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,6 +11,11 @@ import groovy.json.JsonOutput */
 node {
  
     cleanWs()
+    environment {
+        CC_API_KEY="25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
+        CC_REGION="ap-southeast-2"
+        CFN_TEMPLATE_FILE_LOCATION="https://github.com/mkhaled93/test/blob/master/s3.yml"
+    }
     stage "Checkout Code"
     //sh "echo $SCAN_REPOSITORY"
     sh "git clone https://github.com/mkhaled93/test.git/"
