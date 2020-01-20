fdb4c8f6897d5e61952f3fd1db00ddd11d9fa583
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-20 18:34:34 +0200

    ye

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,9 +12,9 @@ node {
  
     cleanWs()
     environment {
-        CC_API_KEY="25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
-        CC_REGION="ap-southeast-2"
-        CFN_TEMPLATE_FILE_LOCATION="https://github.com/mkhaled93/test/blob/master/s3.yml"
+        CC_API_KEY = "25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
+        CC_REGION = "ap-southeast-2"
+        CFN_TEMPLATE_FILE_LOCATION = "https://github.com/mkhaled93/test/blob/master/s3.yml"
     }
     stage "Checkout Code"
     //sh "echo $SCAN_REPOSITORY"
@@ -36,9 +36,9 @@ node {
     sh "pip3 install -r https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/requirements.txt --user"
     sh "wget https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/scanner.py"
     sh "chmod +x test/var.sh"
-    sh "./test/var.sh"
-    //sh "echo $CFN_TEMPLATE_FILE_LOCATION"
-    //sh "python3 scanner.py"
+    //sh "./test/var.sh"
+    sh "echo $CFN_TEMPLATE_FILE_LOCATION"
+    sh "python3 scanner.py"
 
     stage "Build the Environment"
     sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststack --template-body file://test/s3.yml --capabilities CAPABILITY_NAMED_IAM"
