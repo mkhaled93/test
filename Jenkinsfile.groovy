d719d05ffdf3bb116d1065f42194a33ee23a37c6
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-20 17:57:55 +0200

    ye

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -30,9 +30,7 @@ node {
     //sh "sudo su"
     sh "pip3 install -r https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/requirements.txt --user"
     sh "wget https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/scanner.py"
-    sh "CC_API_KEY=''25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2''"
-    sh "CC_REGION=''ap-southeast-2''"
-    sh "CFN_TEMPLATE_FILE_LOCATION=\"https://github.com/mkhaled93/test/blob/master/s3.yml\""
+    sh "./test/var.sh"
     sh "echo $CFN_TEMPLATE_FILE_LOCATION"
     sh "python3 scanner.py"
 
---
+++ var.sh
@@ -0,0 +1,3 @@
+    CC_API_KEY="25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
+    CC_REGION="ap-southeast-2"
+    CFN_TEMPLATE_FILE_LOCATION="https://github.com/mkhaled93/test/blob/master/s3.yml"
