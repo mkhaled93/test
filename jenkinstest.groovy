93f0e7f426e98fdbf10a7d1a640b91c6c7a239b7
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-21 14:06:01 +0200

    ye

---
+++ jenkinstest.groovy
@@ -0,0 +1,110 @@
+/*def SCAN_REPOSITORY=""
+def SMART_CHECK_SERVER=""
+def AWS_REGION=""
+def GIT_REPO=""
+def GIT_CREDENTIALS=""
+def DSSC_CREDENTIALS=""
+def REPO_CREDENTIALS=""
+def SCAN_REGISTRY=""
+def BRANCH_NAME = "master"
+import groovy.json.JsonOutput */
+node {
+ 
+    cleanWs()
+    /*environment {
+        CC_API_KEY = "25MpkR3skTLvjw7zPRnP3AiWNBvnwva5aoMwLPt3ZF8VX2gNE7zS3YGox8UfXGz2"
+        CC_REGION = "ap-southeast-2"
+        CFN_TEMPLATE_FILE_LOCATION = "https://github.com/mkhaled93/test/blob/master/s3.yml"
+        sh " echo $CC_REGION "
+        sh "echo env.CC_REGION"
+    }*/
+    //sh "echo env.CC_API_KEY"
+    stage "Checkout Code"
+    //sh "echo $SCAN_REPOSITORY"
+    sh "git clone https://github.com/mkhaled93/test.git/"
+    //sh "printenv"
+    
+    //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: BRANCH_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '.']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS, url: GIT_REPO]]]
+    stage "Scan"
+    //sh "uname -r"
+    //sh "sudo yum install https://centos7.iuscommunity.org/ius-release.rpm"
+    //sh "sudo yum update"
+    sh "sudo yum install -y python36"
+    sh "sudo python3 --version"
+    sh "sudo curl -O https://bootstrap.pypa.io/get-pip.py"
+    sh "sudo python3 get-pip.py --user"
+    sh "sudo curl -O https://bootstrap.pypa.io/get-pip.py"
+    sh "sudo python3 get-pip.py"
+    //sh "sudo su"
+    sh "pip3 install -r https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/requirements.txt --user"
+    sh "wget https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/scanner.py"
+    sh "chmod +x test/var.sh"
+    //sh "./test/var.sh"
+    sh "echo $CFN_TEMPLATE_FILE_LOCATION"
+    sh "python3 scanner.py"
+
+    stage "Build the Environment"
+    sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststack --template-body file://test/s3.yml --capabilities CAPABILITY_NAMED_IAM"
+    /*sh "docker build -t $SCAN_REPOSITORY ."
+    sh "printenv"
+    stage "Send to Repository"
+    sh "eval \$(aws ecr get-login --no-include-email --region $AWS_REGION | sed 's|https://||')"
+    sh "docker tag $SCAN_REPOSITORY:latest $SCAN_REGISTRY/$SCAN_REPOSITORY:$BUILD_ID"
+    sh "docker push $SCAN_REGISTRY/$SCAN_REPOSITORY:$BUILD_ID"
+ 
+ 
+
+    /*stage "Smart Check"
+
+     def SCAN_IMAGE="$SCAN_REGISTRY/$SCAN_REPOSITORY:$BUILD_ID"
+     sh "echo $SCAN_IMAGE"
+
+
+    withCredentials([
+        usernamePassword([
+            credentialsId: DSSC_CREDENTIALS,
+            usernameVariable: "DSSC_USER",
+            passwordVariable: "DSSC_PASSWORD",
+        ])
+    ]){
+        withCredentials([
+            usernamePassword([
+                credentialsId: REPO_CREDENTIALS,
+            usernameVariable: "Access_ID",
+            passwordVariable: "Secret_ID",
+            ])
+        ]){
+            smartcheckScan([
+                imageName: SCAN_IMAGE,
+                smartcheckHost: SMART_CHECK_SERVER,
+                smartcheckUser: DSSC_USER,
+                smartcheckPassword: DSSC_PASSWORD,
+                insecureSkipTLSVerify: true,
+                findingsThreshold: JsonOutput.toJson([
+                    "malware": 1,
+                    "vulnerabilities": [
+                        "critical": 1,
+                        "high": 1,
+                    ],
+                    "contents": [
+                        "critical": 1,
+                        "high": 1,
+                    ]
+                ]).toString(),
+                imagePullAuth: JsonOutput.toJson([
+                    "aws": [
+						"region": "ap-southeast-1",
+						"accessKeyID": Access_ID,
+						"secretAccessKey": Secret_ID,
+						]
+                ]).toString(),
+            ])
+        }
+    }
+    sh "cat "
+    stage "Certify Release"
+    sh "docker tag $SCAN_REPOSITORY:latest $SCAN_REGISTRY/$SCAN_REPOSITORY:$BRANCH_NAME"
+    stage "Deploy to Production"
+
+    sh "docker push $SCAN_REGISTRY/$SCAN_REPOSITORY:$BRANCH_NAME" */
+}
