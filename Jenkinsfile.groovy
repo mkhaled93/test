9a9fdf6b1f3246c32bc2bbd413e497f5727b1981
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-01-23 12:37:25 +0200

    chnange name

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,25 +7,12 @@ node {
 
     stage "Scan"
 
-    /*sh "sudo yum install -y python36"
-    
-    sh "echo $PATH"
-    sh "export PATH=$PATH:/var/lib/jenkins/.local/bin"
-    sh "echo $PATH"
-    sh "curl -O https://bootstrap.pypa.io/get-pip.py"
-    sh "python3 get-pip.py --user"
-    sh "whoami"
-    sh "pwd"
-    sh "ifconfig" */
     sh "pip3 install -r https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/requirements.txt --user"
     sh "wget https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/scanner.py"
     sh "python3 scanner.py"
-    //archiveArtifacts artifacts: 'findings.json', onlyIfSuccessful: false
-    //archiveArtifacts artifacts: 'findings.json'
-
 
     stage "Build the Environment"
 
-    sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststack --template-body file://$CFN_TEMPLATE_FILE_LOCATION --capabilities CAPABILITY_NAMED_IAM"
+    sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststackk --template-body file://$CFN_TEMPLATE_FILE_LOCATION --capabilities CAPABILITY_NAMED_IAM"
 }
 
